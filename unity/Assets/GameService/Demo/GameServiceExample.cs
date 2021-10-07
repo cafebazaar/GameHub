bdeac1da2f7bafc826d7fb3e57a67aca01d27ed3
Mansour <mansurjavadi@gmail.com>
2021-10-07 21:33:55 +0330

    Add Demo scene

---
+++ unity/Assets/GameService/Demo/GameServiceExample.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Bazaar;
+
+public class GameServiceExample : MonoBehaviour
+{
+    public Text ConsoleText;
+
+    private GameService gameService;
+
+    void Start()
+    {
+        Log("Plugin Version: " + PluginVersion.VersionString);
+        gameService = new GameService();
+    }
+
+    public async void Connect()
+    {
+        var result = await gameService.Connect();
+        Log($"{result.message}, {result.stackTrace}");
+    }
+
+    public async void StartTournamentMatch()
+    {
+        var result = await gameService.StartTournamentMatch("match_id", "metadata");
+        Log($"{result.message}, {result.stackTrace}");
+    }
+
+    public async void EndTournamentMatch()
+    {
+        var result = await gameService.EndTournamentMatch("session_id", 0.4f);
+        Log($"{result.message}, {result.stackTrace}");
+    }
+
+    public void ShowLastTournamentLeaderboard()
+    {
+        gameService.ShowLastTournamentLeaderboard();
+    }
+
+    public void Log(string message)
+    {
+        ConsoleText.text += message + "\n";
+    }
+
+    // void OnApplicationQuit()
+    // {
+    //     gameService.Disconnect();
+    // }
+}
---
+++ unity/Assets/GameService/Scripts/PluginVersion.cs
@@ -0,0 +1,8 @@
+namespace Bazaar
+{
+    public class PluginVersion
+    {
+        public const int VersionInt = 1;
+        public const string VersionString = "1.0.0-alpha01";
+    }
+}
