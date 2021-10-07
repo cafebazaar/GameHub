1d1a036f2b1da2340e36137a9530abc4dae55bbf
Mansour <mansurjavadi@gmail.com>
2021-10-07 21:26:56 +0330

    Service Implementation

--- android/game/src/main/java/com/farsitel/bazaar/games/GamesServiceConnection.java
+++ android/game/src/main/java/com/farsitel/bazaar/games/GamesServiceConnection.java
@@ -18,6 +18,12 @@ public class GamesServiceConnection implements ServiceConnection {
     }
 
     public void onServiceConnected(ComponentName name, IBinder boundService) {
+        try {
+            service = GameService.Stub.asInterface(boundService);
+        } catch (Exception e) {
+            e.printStackTrace();
+            callback.onFinish(STATUS_FAILURE, "Bazaar Game Service connection failed.", e.getMessage());
+        }
 
         Log.e(GameServiceBridge.TAG, "onServiceConnected(): Connected");
         callback.onFinish(STATUS_SUCCESS, "Bazaar Game Service connected.", "");
@@ -35,16 +41,33 @@ public class GamesServiceConnection implements ServiceConnection {
     }
 
     public Bundle startTournamentMatch(ITournamentMatchCallback callback, String matchId, String metaData) {
+        try {
+            Bundle bundle = service.startTournamentMatch(context.getPackageName(), matchId, metaData);
+        } catch (RemoteException e) {
+            callback.onFinish(STATUS_UNKNOWN, "Error on startTournamentMatch", e.getMessage());
+            e.printStackTrace();
+        }
         callback.onFinish(STATUS_SUCCESS, "no data", "");
         return null;
     }
 
     public Bundle endTournamentMatch(ITournamentMatchCallback callback, String sessionId, float coefficient) {
+        try {
+            Bundle bundle = service.endTournamentMatch(sessionId, coefficient);
+        } catch (RemoteException e) {
+            callback.onFinish(STATUS_UNKNOWN, "Error on startTournamentMatch", e.getMessage());
+            e.printStackTrace();
+        }
 
         callback.onFinish(STATUS_SUCCESS, "no data", "");
         return null;
     }
 
     public void showLastTournamentLeaderboard() {
+        try {
+            service.showLastTournamentLeaderboard();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
